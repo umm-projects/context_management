823a11dc2fe7a5f164776cb6130746b43b9fe006
Tetsuya Mori <monry84@gmail.com>
2018-05-11 14:47:18 +0900

    :shirt: Fix code style
    * Likely Microsoft:

--- Assets/Scripts/UnityModule/ContextManagement/ProjectContext.cs
+++ Assets/Scripts/UnityModule/ContextManagement/ProjectContext.cs
@@ -1,60 +1,53 @@
-﻿using System;
+﻿using JetBrains.Annotations;
 using UnityEngine;
-
 #pragma warning disable 649
 
-namespace UnityModule.ContextManagement {
-
-    public interface IProjectContext {
-
+namespace UnityModule.ContextManagement
+{
+    [PublicAPI]
+    public interface IProjectContext
+    {
         string Name { get; }
 
         // 本来であれば extension method にしたいところだが、 il2cpp の AOT 制約により enum な extension method は NG なのでこちらに定義する
         string CreateSceneName<TEnum>(TEnum sceneName) where TEnum : struct;
-
     }
 
-    public class ProjectContext : IProjectContext {
+    public class ProjectContext : IProjectContext
+    {
+        [SerializeField] private string name;
 
-        [SerializeField]
-        private string name;
-
-        public string Name {
-            get {
-                return this.name;
-            }
+        public string Name
+        {
+            get { return name; }
         }
 
-        [SerializeField]
-        private string sceneNamePrefix;
+        [SerializeField] private string sceneNamePrefix;
 
-        public string SceneNamePrefix {
-            get {
-                return this.sceneNamePrefix;
-            }
+        public string SceneNamePrefix
+        {
+            get { return sceneNamePrefix; }
         }
 
-        [SerializeField]
-        private string namespacePrefix;
+        [SerializeField] private string namespacePrefix;
 
-        public string NamespacePrefix {
-            get {
-                return this.namespacePrefix;
-            }
+        public string NamespacePrefix
+        {
+            get { return namespacePrefix; }
         }
 
-        public string CreateSceneName<TEnum>(TEnum sceneName) where TEnum : struct {
-            return string.Format("{0}{1}", this.SceneNamePrefix, sceneName.ToString());
+        public string CreateSceneName<TEnum>(TEnum sceneName) where TEnum : struct
+        {
+            return string.Format("{0}{1}", SceneNamePrefix, sceneName.ToString());
         }
-
     }
 
-    public static class ProjectContextExtension {
-
-        public static TProjectContext As<TProjectContext>(this IProjectContext projectContext) where TProjectContext : class, IProjectContext {
+    [PublicAPI]
+    public static class ProjectContextExtension
+    {
+        public static TProjectContext As<TProjectContext>(this IProjectContext projectContext) where TProjectContext : class, IProjectContext
+        {
             return projectContext as TProjectContext;
         }
-
     }
-
 }
